577cd74cdd284916a7c268098c38ce7d5d455a04
BabaYaGa74 <beeplovenepal74@gmail.com>
2024-08-03 14:51:26 +0545

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import Navbar from "./Navbar/Navbar"
+import Category from "./Components/Category/Category";
+import Category2 from "./Components/Category/Category2";
+import Hero from "./Components/Hero/Hero";
+import Navbar from "./Components/Navbar/Navbar"
 
 export default function App() {
     return (
         <div >
             <Navbar/>
+            <Hero/>
+            <Category/>
+            <Category2/>
         </div>
     );
 }
